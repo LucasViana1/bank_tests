486728bb8e2d029018d2a369b416eb05bab69d76
lucas <you@example.com>
2020-11-26 00:04:21 -0300

    created the tests for transactions

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-import App from './App';
+import App, {calcularNovoSaldo} from './App';
 
 describe('Componente principal', ()=>{
   it('Mostrar o nome do banco', ()=>{
@@ -9,4 +9,62 @@ describe('Componente principal', ()=>{
 
     expect(screen.getByText('ByteBank')).toBeInTheDocument();
   })
+})
+
+describe('Quando eu realizo uma transação', ()=>{
+  it('Que é um saque, o valor vai diminuir', ()=>{
+    const valores = {
+      transacao: 'saque',
+      valor: 50
+    }
+    const novoSaldo = calcularNovoSaldo(valores, 150);
+
+    expect(novoSaldo).toBe(100);
+  })
+
+  it('Que é um deposito, o valor vai aumentar', ()=>{
+    const valores = {
+      transacao: 'deposito',
+      valor: 50
+    }
+
+    const novoSaldo = calcularNovoSaldo(valores, 150);
+
+    expect(novoSaldo).toBe(200);
+  })
+
+  it('que é um saque, a transação deve ser realizada', ()=>{
+    render(<App />);
+
+    const saldo = screen.getByText('R$ 1000');
+    const transacao = screen.getByLabelText('Saque');
+    const valor = screen.getByTestId('valor');
+    const botaoTransacao = screen.getByText('Realizar operação')
+
+    expect(saldo.textContent).toBe('R$ 1000');
+
+    fireEvent.click(transacao, { target: { value: 'saque' } });
+    fireEvent.change(valor, { target: { value: 10 } });
+    fireEvent.click(botaoTransacao);
+
+    expect(saldo.textContent).toBe('R$ 990');
+  })
+
+  // FORMA ANTIGA
+  // it('que é um saque, a transação deve ser realizada', ()=>{
+  //   const {getByText, getByTestId, getByLabelText} = render(<App />);
+
+  //   const saldo = getByText('R$ 1000');
+  //   const transacao = getByLabelText('Saque');
+  //   const valor = getByTestId('valor');
+  //   const botaoTransacao = getByText('Realizar operação')
+
+  //   expect(saldo.textContent).toBe('R$ 1000');
+
+  //   fireEvent.click(transacao, { target: { value: 'saque' } });
+  //   fireEvent.change(valor, { target: { value: 10 } });
+  //   fireEvent.click(botaoTransacao);
+
+  //   expect(saldo.textContent).toBe('R$ 990');
+  // })
 })
